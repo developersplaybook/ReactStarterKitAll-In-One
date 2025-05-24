5a12d130629d30d7c4b324c6e623018c6d5e3401
Gunnar Siréus <admin.playbook@sireus.se>
2025-05-24 19:04:24 +0200

    boot-client.jsx without server side rendering

--- ClientApp/boot-client.jsx
+++ ClientApp/boot-client.jsx
@@ -1,17 +1,8 @@
-import * as React from 'react';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+// index.jsx (eller main.jsx)
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const renderApp = () => {
-  const container = document.getElementById("react-app");
-
-  if (container.hasChildNodes()) {
-    // If the container already has content, it means we're hydrating
-    hydrateRoot(container, <App />);
-  } else {
-    // Otherwise, we're just rendering
-    createRoot(container).render(<App />);
-  }
-}
-
-renderApp();
+const container = document.getElementById('react-app');
+const root = createRoot(container);
+root.render(<App />);
